1c7170ad404645b096caa6532d9b29c00bfc45b0
Dan Vega <danvega@gmail.com>
2021-08-17 16:29:25 -0400

    initial commit

---
+++ progressive/src/test/java/dev/danvega/progressive/ProgressiveApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danvega.progressive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
